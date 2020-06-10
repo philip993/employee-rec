5c2f660d83f93ae3cdf9b5443d6ea390d924433d
Filip FICO <fitz993m@gmail.com>
2020-06-10 22:03:12 +0200

    2.10. Updated App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+
+// Styles
+import Styles from '../Style/Style';
 import './App.css';
+// React Router Dom
+import { Switch, Route } from 'react-router-dom';
+// React Components
+import Employee from '../Employee/Employee';
+import Homepage from '../Homepage/Homepage';
 
 const App = () => {
+  const classes = Styles();
   return (
-    <div>
-      <h1>Employee List</h1>
+    <div className={classes.root}>
+      <Switch>
+        <Route path="/" exact component={Homepage} />
+        <Route path="/employees" exact component={Employee} />
+      </Switch>
     </div>
   );
 };
