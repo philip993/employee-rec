1269970420109a904866e5a02c9ad85f760b6131
Filip FICO <fitz993m@gmail.com>
2020-06-10 22:33:43 +0200

    2.12. Added into App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,11 +8,13 @@ import { Switch, Route } from 'react-router-dom';
 // React Components
 import Employee from '../Employee/Employee';
 import Homepage from '../Homepage/Homepage';
+import Navbar from '../Navbar/Navbar';
 
 const App = () => {
   const classes = Styles();
   return (
     <div className={classes.root}>
+      <Navbar />
       <Switch>
         <Route path="/" exact component={Homepage} />
         <Route path="/employees" exact component={Employee} />
