af49cf009af318e5d0de2f08e566901031946ebf
Filip FICO <fitz993m@gmail.com>
2020-06-10 21:59:44 +0200

    2.6. Material-Ui & React-Router-Dom setup

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <Router>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </Router>,
+
   document.getElementById('root')
 );
