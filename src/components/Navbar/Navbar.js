5cbbca975e34a9e1f3a926c36c1d9bc8e1255ed2
Filip FICO <fitz993m@gmail.com>
2020-06-10 22:33:25 +0200

    2.11. Created Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+// Styles
+import Styles from '../Style/Style';
+// React Router Dom
+import { Link } from 'react-router-dom';
+// Mateterial Ui
+import { Button } from '@material-ui/core';
+
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const classes = Styles();
+
+  return (
+    <div className={classes.header}>
+      <Button>
+        <Link to="/">Home</Link>
+      </Button>
+      <Button>
+        <Link to="/employees">Employee List</Link>
+      </Button>
+      <Button>
+        <Link to="/contact">Contact</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default Navbar;
