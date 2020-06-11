9dad2b3e49e33580649f0509227b5c7008ab3dfb
Filip Mihajlovski <fitz993m@gmail.com>
2020-06-11 14:32:21 +0200

    3.5. Updated reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 
 // Import Reducers
 import { EmployeeReducer } from '../components/Employee/EmployeeReducer';
+import { DetailsReducer } from '../components/Details/DetailsReducer';
 
 const rootReducer = combineReducers({
   EmployeeReducer,
+  DetailsReducer,
 });
 
 export default rootReducer;
