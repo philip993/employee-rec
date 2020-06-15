49b78b786ac865b0bfdba72758c23f167f0a9fc7
Filip FICO <fitz993m@gmail.com>
2020-06-15 22:55:14 +0200

    4.5. New layout for form

--- src/components/AddEmployee/AddEmployee.js
+++ src/components/AddEmployee/AddEmployee.js
@@ -3,13 +3,16 @@ import { useSelector, useDispatch } from 'react-redux';
 
 // Styles
 import Styles from '../Style/Style';
+// React Component
+import EmploymentInfo from './EmploymentInfo';
 // Redux Actions
 import {} from './AddEmployeeActions';
 // Material-Ui
 import { Typography, FormGroup, InputBase } from '@material-ui/core';
+import PersonalInfo from './PersonalInfo';
 
 const AddEmployee = () => {
-  const addEmp = useSelector((state) => state.AddEmployeeReducer);
+  const { step } = useSelector((state) => state.AddEmployeeReducer);
   const dispatch = useDispatch();
   const classes = Styles();
 
@@ -21,16 +24,7 @@ const AddEmployee = () => {
   return (
     <div className={classes.pageContainer}>
       <Typography variant="h4">Add New Employee</Typography>
-      <form>
-        <FormGroup>
-          <InputBase
-            type="text"
-            value={'val'}
-            onChange={'func'}
-            placeholder="First Name here.."
-          />
-        </FormGroup>
-      </form>
+      {step === 1 ? <PersonalInfo /> : step === 2 ? <EmploymentInfo /> : 'xxx'}
     </div>
   );
 };
