970e4ed465a4688cf8540e0f561c1607620dad76
Filip FICO <fitz993m@gmail.com>
2020-06-17 00:44:29 +0200

    4.17.   Updated Add Employee for Address Info Comp

--- src/components/AddEmployee/AddEmployee.js
+++ src/components/AddEmployee/AddEmployee.js
@@ -5,26 +5,30 @@ import { useSelector, useDispatch } from 'react-redux';
 import Styles from '../Style/Style';
 // React Component
 import EmploymentInfo from './EmploymentInfo';
+import PersonalInfo from './PersonalInfo';
+import AddressInfo from './AddressInfo';
 // Redux Actions
 import {} from './AddEmployeeActions';
 // Material-Ui
-import { Typography, FormGroup, InputBase } from '@material-ui/core';
-import PersonalInfo from './PersonalInfo';
+import { Typography } from '@material-ui/core';
 
 const AddEmployee = () => {
   const { step } = useSelector((state) => state.AddEmployeeReducer);
   const dispatch = useDispatch();
   const classes = Styles();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('submited');
-  };
-
   return (
     <div className={classes.pageContainer}>
       <Typography variant="h4">Add New Employee</Typography>
-      {step === 1 ? <PersonalInfo /> : step === 2 ? <EmploymentInfo /> : 'xxx'}
+      {step === 1 ? (
+        <PersonalInfo />
+      ) : step === 2 ? (
+        <EmploymentInfo />
+      ) : step === 3 ? (
+        <AddressInfo />
+      ) : (
+        'xxx'
+      )}
     </div>
   );
 };
