b1f8b6c886fda08076acababa95b6f0496a1386d
Filip FICO <fitz993m@gmail.com>
2020-06-18 00:03:31 +0200

    4.30. Stepper added

--- src/components/AddEmployee/AddEmployee.js
+++ src/components/AddEmployee/AddEmployee.js
@@ -3,25 +3,57 @@ import { useSelector, useDispatch } from 'react-redux';
 
 // Styles
 import Styles from '../Style/Style';
+// React Router Dom
+import { useHistory } from 'react-router-dom';
 // React Component
 import EmploymentInfo from './EmploymentInfo';
 import PersonalInfo from './PersonalInfo';
 import AddressInfo from './AddressInfo';
+import ContactInfo from './ContactInfo';
+import IdentityInfo from './IdentityInfo';
 // Redux Actions
 import {} from './AddEmployeeActions';
 // Material-Ui
-import { Typography } from '@material-ui/core';
-import ContactInfo from './ContactInfo';
-import IdentityInfo from './IdentityInfo';
+import { Typography, Stepper, Step, StepLabel } from '@material-ui/core';
+import Finish from './Finish';
+import AddEmployeeError from './AddEmployeeError';
 
 const AddEmployee = () => {
-  const { step } = useSelector((state) => state.AddEmployeeReducer);
+  const { step, activeStep, errors } = useSelector(
+    (state) => state.AddEmployeeReducer
+  );
   const dispatch = useDispatch();
   const classes = Styles();
+  const history = useHistory();
 
   return (
     <div className={classes.pageContainer}>
+      {errors === null
+        ? ''
+        : errors === false
+        ? history.push('/addsuccess')
+        : history.push('/addfail')}
       <Typography variant="h4">Add New Employee</Typography>
+      <Stepper activeStep={activeStep} alternativeLabel>
+        <Step activeStep={activeStep === 0}>
+          <StepLabel>Personal Information</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Eployment Information</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Address Information</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Identity Information</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Contact Information</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Complete Request</StepLabel>
+        </Step>
+      </Stepper>
       {step === 1 ? (
         <PersonalInfo />
       ) : step === 2 ? (
@@ -33,7 +65,7 @@ const AddEmployee = () => {
       ) : step === 5 ? (
         <IdentityInfo />
       ) : (
-        'Finish'
+        <Finish />
       )}
     </div>
   );
