0d797becfb0adf8de692d1b348d1ee2793529041
Filip Mihajlovski <fitz993m@gmail.com>
2020-06-22 13:46:49 +0200

    5.8. Routes for GET and PUT req

--- src/backend/Shift/ShiftRoute.js
+++ src/backend/Shift/ShiftRoute.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const shiftCtrl = require('./ShiftController');
 
-router.post('/shifts/add', shiftCtrl.createShift);
+router.post('/add', shiftCtrl.createShift);
+router.get('/', shiftCtrl.getShifts);
+router.put('/update', shiftCtrl.changeShifts);
 
 module.exports = router;
