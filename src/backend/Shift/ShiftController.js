c0e99f4016b9e6f92e6804e1ce3042d03c2d317e
Filip Mihajlovski <fitz993m@gmail.com>
2020-06-22 13:46:27 +0200

    5.7. GET and PUT request

--- src/backend/Shift/ShiftController.js
+++ src/backend/Shift/ShiftController.js
@@ -1,9 +1,15 @@
 const Shift = require('./ShiftModel');
+const db = require('../database/db');
 
 exports.createShift = (req, res) => {
-  let {} = req.body;
+  let { employeeId, shiftCode, startDate, endDate } = req.body;
 
-  Shift.create({})
+  Shift.create({
+    employeeId,
+    shiftCode,
+    startDate,
+    endDate,
+  })
     .then((shift) => {
       res.status(201).json({
         shift,
@@ -15,4 +21,27 @@ exports.createShift = (req, res) => {
         msg: 'System Error. Cannot create shift schedule.',
       });
     });
+};
+
+exports.getShifts = (req, res) => {
+  Shift.findAll()
+    .then((shifts) => {
+      res.status(200).json({
+        shifts,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        msg: 'System Error. Cannot load Shift Schedules',
+      });
+    });
+};
+
+exports.changeShifts = (req, res) => {
+  res.send(
+    db.query(
+      'UPDATE shifts SET shift_code = (CASE WHEN shift_code = "first" THEN "second" WHEN shift_code = "second" THEN "third" WHEN shift_code = "third" THEN "first" end)'
+    )
+  );
 };
