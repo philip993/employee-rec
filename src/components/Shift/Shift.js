34b7a2e8c63ce65e6fe8a755401526ff37b53380
Filip Mihajlovski <fitz993m@gmail.com>
2020-06-22 15:39:22 +0200

    5.14. Added table layout

--- src/components/Shift/Shift.js
+++ src/components/Shift/Shift.js
@@ -6,7 +6,14 @@ import Styles from '../Style/Style';
 // Redux Actions
 import { requestGetShifts } from './ShiftActions';
 // Material Ui
-import { Typography } from '@material-ui/core';
+import {
+  Typography,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from '@material-ui/core';
 
 const Shift = () => {
   const { shiftSchedule } = useSelector((state) => state.ShiftReducer);
@@ -20,16 +27,30 @@ const Shift = () => {
   return (
     <div>
       <Typography variant="h4">Shift Schedule</Typography>
-      {shiftSchedule
-        .sort((a, b) =>
-          a.shiftCode > b.shiftCode ? 1 : b.shiftCode > a.shiftCode ? -1 : 0
-        )
-        .map(({ id, employeeId, shiftCode }) => (
-          <div key={id}>
-            <h4>{employeeId}</h4>
-            <h4>{shiftCode}</h4>
-          </div>
-        ))}
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>EMPLOYEE ID</TableCell>
+            <TableCell>START DATE</TableCell>
+            <TableCell>END DATE</TableCell>
+            <TableCell>SHIFT</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {shiftSchedule
+            .sort((a, b) =>
+              a.shiftCode > b.shiftCode ? 1 : b.shiftCode > a.shiftCode ? -1 : 0
+            )
+            .map(({ id, employeeId, shiftCode, startDate, endDate }) => (
+              <TableRow key={id}>
+                <TableCell>{employeeId}</TableCell>
+                <TableCell>{startDate}</TableCell>
+                <TableCell>{endDate}</TableCell>
+                <TableCell>{shiftCode}</TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
     </div>
   );
 };
