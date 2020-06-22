c1a276802af7a43d6028acba75a3fbf4dff3bd84
Filip Mihajlovski <fitz993m@gmail.com>
2020-06-22 13:46:01 +0200

    5.6. Updated model

--- src/backend/Shift/ShiftModel.js
+++ src/backend/Shift/ShiftModel.js
@@ -1,27 +1,34 @@
 const { Sequelize } = require('sequelize');
 const db = require('../database/db');
 
-const Shift = db.define('shift', {
-  id: {
-    type: Sequelize.INTEGER,
-    references: 'employees',
-    refencesKey: 'id',
+const Shift = db.define(
+  'shift',
+  {
+    employeeId: {
+      type: Sequelize.INTEGER,
+      references: 'employees',
+      refencesKey: 'id',
+      field: 'employee_id',
+    },
+    shiftCode: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      field: 'shift_code',
+    },
+    startDate: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      field: 'start_date',
+    },
+    endDate: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      field: 'end_date',
+    },
   },
-  shiftCode: {
-    type: Sequelize.toString,
-    allowNull: false,
-    field: 'shift_code',
-  },
-  startDate: {
-    type: Sequelize.DATE,
-    allowNull: false,
-    field: 'start_date',
-  },
-  endDate: {
-    type: Sequelize.DATE,
-    allowNull: false,
-    field: 'end_date',
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
 module.exports = Shift;
