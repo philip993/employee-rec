607cb842a0aff94fb486cc15c04cd0c26eceda9e
Filip FICO <fitz993m@gmail.com>
2020-06-24 16:25:05 +0200

    5.22. Added date to saved PDF file

--- src/components/Pdf/Pdf.js
+++ src/components/Pdf/Pdf.js
@@ -12,11 +12,15 @@ import { Button } from '@material-ui/core';
 import GetAppIcon from '@material-ui/icons/GetApp';
 
 const Pdf = () => {
+  const day = new Date().getDate();
+  const month = new Date().getMonth();
+  const year = new Date().getFullYear();
+  const date = `${day}.${month}.${year}`;
   const savePDF = () => {
     window.html2canvas = html2canvas;
     var doc = new jsPDF();
     doc.autoTable({ html: '#table' });
-    doc.save('shift-schedule.pdf');
+    doc.save(`shift-schedule ${date}.pdf`);
   };
   const classes = Styles();
 
