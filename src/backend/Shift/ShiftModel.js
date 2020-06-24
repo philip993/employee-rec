61cfdc554beac4c54ef86af184f4e5e848c7f3fb
Filip FICO <fitz993m@gmail.com>
2020-06-24 23:48:18 +0200

    5.27. Changes in model

--- src/backend/Shift/ShiftModel.js
+++ src/backend/Shift/ShiftModel.js
@@ -11,17 +11,19 @@ const Shift = db.define(
       field: 'employee_id',
     },
     shiftCode: {
-      type: Sequelize.STRING,
+      type: Sequelize.ENUM({
+        values: ['first', 'second', 'third'],
+      }),
       allowNull: false,
       field: 'shift_code',
     },
     startDate: {
-      type: Sequelize.DATE,
+      type: Sequelize.DATEONLY,
       allowNull: false,
       field: 'start_date',
     },
     endDate: {
-      type: Sequelize.DATE,
+      type: Sequelize.DATEONLY,
       allowNull: false,
       field: 'end_date',
     },
