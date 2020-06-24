8be332d5644bd51f05651fdb7f6f1dc9ff3e4d98
Filip FICO <fitz993m@gmail.com>
2020-06-24 23:46:44 +0200

    5.25. Updated root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -5,12 +5,14 @@ import { EmployeeReducer } from '../components/Employee/EmployeeReducer';
 import { DetailsReducer } from '../components/Details/DetailsReducer';
 import { AddEmployeeReducer } from '../components/AddEmployee/AddEmployeeReducer';
 import { ShiftReducer } from '../components/Shift/ShiftReducer';
+import { UpdateShiftReducer } from '../components/UpdateShift/UpdateShiftReducer';
 
 const rootReducer = combineReducers({
   EmployeeReducer,
   DetailsReducer,
   AddEmployeeReducer,
   ShiftReducer,
+  UpdateShiftReducer,
 });
 
 export default rootReducer;
