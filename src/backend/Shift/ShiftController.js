d60f8697b81195f746084600d388080f754e8f62
Filip FICO <fitz993m@gmail.com>
2020-06-25 16:46:05 +0200

    5.33. Query to change date 7 days

--- src/backend/Shift/ShiftController.js
+++ src/backend/Shift/ShiftController.js
@@ -50,6 +50,10 @@ exports.changeShifts = (req, res) => {
   res.send(
     db.query(
       'UPDATE shifts SET shift_code = (CASE WHEN shift_code = "first" THEN "second" WHEN shift_code = "second" THEN "third" WHEN shift_code = "third" THEN "first" end)'
-    )
+    ),
+    db.query(
+      'UPDATE shifts SET start_date=DATE_ADD(start_date, INTERVAL 7 DAY)'
+    ),
+    db.query('UPDATE shifts SET end_date=DATE_ADD(end_date, INTERVAL 7 DAY)')
   );
 };
