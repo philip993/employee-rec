c0821312d10793cec8a1660905ffa0c5e0bad0ca
Filip Mihajlovski <fitz993m@gmail.com>
2020-07-03 15:48:18 +0200

    6.13. Show number of meals in each shift - 1 opt.

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -6,7 +6,16 @@ import Style from '../Style/Style';
 // Redux Actions
 import { requestMealSchedule } from './MealActions';
 // Material Ui
-import { Typography, Button } from '@material-ui/core';
+import {
+  Typography,
+  Button,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  TableFooter,
+} from '@material-ui/core';
 
 const Meals = () => {
   const { mealsSchedule } = useSelector((state) => state.MealsReducer);
@@ -20,16 +29,53 @@ const Meals = () => {
   return (
     <div>
       <Typography variant="h4">Meals Schedule</Typography>
-      {mealsSchedule
-        .filter((empl, index) => empl.shifts[0].shiftCode === 'third')
-        .map((meal) => (
-          <div>
-            <h4>{meal.id}</h4>
-            <h4>{meal.employees[0].firstName}</h4>
-            <h4>{meal.shifts[0].shiftCode}</h4>
-            <h4>{meal.shifts.length}</h4>
-          </div>
-        ))}
+      <Table className={classes.mealTable}>
+        <TableHead>
+          <TableRow className={classes.mealSub}>
+            <TableCell className={classes.mealCellTwo}>SHIFT</TableCell>
+            <TableCell className={classes.mealCell}>COUNT</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell className={classes.mealCellTwo}>FIRST</TableCell>
+            <TableCell className={classes.mealCell}>
+              {mealsSchedule
+                .filter((s) => s.shifts[0].shiftCode === 'first')
+                .map((arr) => arr.mealCount)
+                .reduce((a, b) => a + b, 0)}
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className={classes.mealCellTwo}>SECOND</TableCell>
+            <TableCell className={classes.mealCell}>
+              {mealsSchedule
+                .filter((s) => s.shifts[0].shiftCode === 'second')
+                .map((arr) => arr.mealCount)
+                .reduce((a, b) => a + b, 0)}
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className={classes.mealCellTwo}>THIRD</TableCell>
+            <TableCell className={classes.mealCell}>
+              {mealsSchedule
+                .filter((s) => s.shifts[0].shiftCode === 'third')
+                .map((arr) => arr.mealCount)
+                .reduce((a, b) => a + b, 0)}
+            </TableCell>
+          </TableRow>
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell className={classes.mealCellFooter}>TOTAL</TableCell>
+            <TableCell className={classes.mealCellFooter}>
+              {mealsSchedule
+                .map((arr) => arr.mealCount)
+                .reduce((a, b) => a + b, 0)}
+            </TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
     </div>
   );
 };
