6ed6d2f1809f3f132b575ebb62f65e12bbb642e1
Filip FICO <fitz993m@gmail.com>
2020-07-07 22:20:41 +0200

    7.1. Contract component

---
+++ src/components/Contract/Contract.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {} from 'react-redux';
+
+// Style
+import Style from '../Style/Style';
+// Redux Actions
+// Material Ui
+import { Typography } from '@material-ui/core';
+
+const Contract = () => {
+  return (
+    <div>
+      <Typography variant="h4">Contract</Typography>
+    </div>
+  );
+};
+
+export default Contract;
