97576b1c7ae2221e243bf36890e83804f3110bf3
Filip Mihajlovski <fitz993m@gmail.com>
2020-07-07 15:18:38 +0200

    6.16. Remove files client/server side

--- src/backend/Meal/MealRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const mealCtrl = require('./MealController');
-
-router.get('/', mealCtrl.getMeals);
-router.post('/add', mealCtrl.createMeals);
-
-module.exports = router;
