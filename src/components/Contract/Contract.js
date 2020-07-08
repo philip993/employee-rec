9b1b5429b3e8025e40df1adb6ad86b321d45c993
Filip FICO <fitz993m@gmail.com>
2020-07-08 23:58:04 +0200

    7.12.  Changes

--- src/components/Contract/Contract.js
+++ src/components/Contract/Contract.js
@@ -9,7 +9,9 @@ import { requestGetContracts } from './ContractActions';
 import { Typography } from '@material-ui/core';
 
 const Contract = () => {
-  const { contractList } = useSelector((state) => state.ContractReducer);
+  const { contractList } = useSelector((state) => ({
+    ...state.ContractReducer,
+  }));
   const dispatch = useDispatch();
   const classes = Style();
 
@@ -20,9 +22,12 @@ const Contract = () => {
   return (
     <div>
       <Typography variant="h4">Contract</Typography>
-      {contractList.map((contract) => (
+      {contractList.map((contract, index) => (
         <div key={contract.id}>
           <h5>{contract.contractId}</h5>
+          <h5>
+            {contract.employees[0].firstName} {contract.employees[0].secondName}
+          </h5>
           <h5>{contract.contractStart}</h5>
           <h5>{contract.contractEnd}</h5>
         </div>
