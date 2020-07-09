7528bb7f4eec2a33bdeb49c87996e1f2fc036220
Filip FICO <fitz993m@gmail.com>
2020-07-10 01:31:29 +0200

    7.15. Show fired employees

--- src/components/Contract/Contract.js
+++ src/components/Contract/Contract.js
@@ -19,42 +19,56 @@ const Contract = () => {
     dispatch(requestGetContracts());
   }, []);
 
+  const handleUpdateContract = (e) => {
+    console.log(e);
+  };
+
   return (
     <div>
       <Typography variant="h4">Contract</Typography>
-      {contractList.map((contract, index) => (
-        <div key={contract.id}>
-          <h5>{contract.contractId}</h5>
-          <h5>
-            {contract.employees[0].firstName} {contract.employees[0].secondName}
-          </h5>
-          <h5>{contract.contractStart}</h5>
-          <h5>{contract.contractEnd}</h5>
-          <h5 hidden disabled>
-            {
-              (contract.daysLeft = Math.floor(
-                Math.ceil(
-                  (new Date(contract.contractEnd) - currentDate) /
-                    (1000 * 60 * 60 * 24)
-                )
-              ))
-            }
-          </h5>
-          <h5>
-            {contract.daysLeft < 0 ? (
-              <div style={{ color: 'red' }}>
-                <p>{contract.daysLeft}</p>
-                <p>EXPIRED</p>
-              </div>
-            ) : (
-              <div style={{ color: 'green' }}>
-                <p>{contract.daysLeft}</p>
-                <p>VALID</p>
-              </div>
-            )}
-          </h5>
-        </div>
-      ))}
+      {contractList.map((contract, index) =>
+        contract.activeContract === true ? (
+          <div key={contract.id}>
+            <h5>
+              {contract.contractId}. {contract.employees[0].firstName}{' '}
+              {contract.employees[0].secondName}
+            </h5>
+            <h5>{contract.contractStart}</h5>
+            <h5>{contract.contractEnd}</h5>
+            <h5 hidden disabled>
+              {
+                (contract.daysLeft = Math.floor(
+                  Math.ceil(
+                    (new Date(contract.contractEnd) - currentDate) /
+                      (1000 * 60 * 60 * 24)
+                  )
+                ))
+              }
+            </h5>
+            <h5>
+              {contract.daysLeft < 0 ? (
+                <div style={{ color: 'red' }}>
+                  <p>{contract.daysLeft}</p>
+                  <p>EXPIRED</p>
+                </div>
+              ) : (
+                <div style={{ color: 'green' }}>
+                  <p>{contract.daysLeft}</p>
+                  <p>VALID</p>
+                </div>
+              )}
+            </h5>
+          </div>
+        ) : (
+          <div key={contract.id}>
+            <p>
+              {contract.id}. {contract.employees[0].firstName}{' '}
+              {contract.employees[0].secondName}
+            </p>
+            <p>FIRED</p>
+          </div>
+        )
+      )}
     </div>
   );
 };
