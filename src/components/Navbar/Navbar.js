ca9c5613cc835f3b6f70a352ba3fb9e8eac6f9e3
Filip Mihajlovski <fitz993m@gmail.com>
2020-07-13 15:44:33 +0200

    7.23. Removed Update Contract from Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -41,9 +41,6 @@ const Navbar = () => {
       <Button>
         <Link to="/addcontract">Add Contract</Link>
       </Button>
-      <Button>
-        <Link to="/updatecontract">Update Contract</Link>
-      </Button>
     </div>
   );
 };
