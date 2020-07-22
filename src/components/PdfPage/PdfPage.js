51f62e0ffb727535d1f77df568a97f31b40f809c
Filip Mihajlovski <fitz993m@gmail.com>
2020-07-22 15:49:19 +0200

    7.58. Adj. print options - in progress

--- src/components/PdfPage/PdfPage.js
+++ src/components/PdfPage/PdfPage.js
@@ -19,9 +19,25 @@ const PdfPage = () => {
   const date = `${day}.${month}.${year}`;
   const savePDF = () => {
     window.html2canvas = html2canvas;
-    var doc = new jsPDF();
-    doc.fromHTML(document.getElementById('pdf-table'));
-    doc.save(`shift-schedule ${date}.pdf`);
+    // var doc = new jsPDF();
+    // doc.fromHTML(document.getElementById('pdf-table'), 10, 10, {
+    //   width: '70',
+    // });
+    // doc.save(`shift-schedule ${date}.pdf`);
+
+    // html2canvas(document.getElementById('pdf-table')).then(function (canvas) {
+    //   var img = canvas.toDataURL('image/png');
+    //   var doc = new jsPDF('p', 'mm', [500, 1000]);
+    //   doc.addImage(img, 'PNG', 10, 10);
+    //   doc.save('test.pdf');
+    // });
+
+    html2canvas(document.getElementById('pdf-table')).then(function (canvas) {
+      var img = canvas.toDataURL('image/png');
+      var doc = new jsPDF('p', 'mm', 'a4');
+      doc.addImage(img, 'JPEG', 0, 0);
+      doc.save('test.pdf');
+    });
   };
   const classes = Styles();
 
