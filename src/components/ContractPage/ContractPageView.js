d2077b8abb56b90ba3a000451ab3003ddf15deeb
Filip FICO <fitz993m@gmail.com>
2020-07-22 20:11:35 +0200

    7.60. Generate contract based on emp. status

--- src/components/ContractPage/ContractPageView.js
+++ src/components/ContractPage/ContractPageView.js
@@ -16,7 +16,7 @@ const ContractPageView = ({
   const classes = Style();
   const currentDate = new Date().getFullYear();
 
-  return (
+  return activeContract ? (
     <Card id="pdf-table" className={classes.contractCard}>
       <div className={classes.contractCompany}>
         <Avatar className={classes.contractAvatar}>VT</Avatar>
@@ -50,6 +50,54 @@ const ContractPageView = ({
             employer will provide conditions suitable for work and pay the
             amount specified above. Also the employee will do all of his/her
             responsibilities described in the job post.
+          </Typography>
+          <div className={classes.contractSignLeft}>
+            <Typography variant="h6">COMPANY SIGNATURE</Typography>
+            <Typography>______________</Typography>
+          </div>
+          <div className={classes.contractSignRight}>
+            <Typography variant="h6">EMPLOYEE SIGNATURE</Typography>
+            <Typography>______________</Typography>
+          </div>
+        </div>
+        <div className={classes.contractDate}>
+          <Typography variant="p">{currentDate}</Typography>
+        </div>
+      </CardContent>
+    </Card>
+  ) : (
+    <Card id="pdf-table" className={classes.contractCard}>
+      <div className={classes.contractCompany}>
+        <Avatar className={classes.contractAvatar}>VT</Avatar>
+        <Typography variant="h6">VIRTUAL TRADE COMPANY</Typography>
+        <Typography variant="p">Lakeside 1B, Ohio, US</Typography>
+      </div>
+
+      <CardContent className={classes.contractPersonal}>
+        <Typography variant="h4" className={classes.contractSub}>
+          CONTRACT TERMINATION
+        </Typography>
+        <Typography variant="h5">EMPLOYEE ID #{contractId}</Typography>
+        <div className={classes.contractInfoLeft}>
+          <Typography variant="h5">
+            {employees[0].firstName} {employees[0].secondName}
+          </Typography>
+          <Typography variant="h6">
+            {employees[0].address}, {employees[0].city}, {employees[0].state}
+          </Typography>
+        </div>
+
+        <div className={classes.contractInfoRight}>
+          <Typography variant="h6">start date: {contractStart}</Typography>
+          <Typography variant="h6">end date: {contractEnd}</Typography>
+        </div>
+        <div className={classes.contractDetails}>
+          <Typography variant="h6">
+            Contract with id number # {id} between VIRTUAL COMPANY and
+            {employees[0].firstName} {employees[0].secondName} has been
+            terminated. Employeer will pay severance package to the employee.
+            The employee can accept or forfeit severance package, which
+            acknoledges the termination of their contract.{' '}
           </Typography>
           <div className={classes.contractSignLeft}>
             <Typography variant="h6">COMPANY SIGNATURE</Typography>
